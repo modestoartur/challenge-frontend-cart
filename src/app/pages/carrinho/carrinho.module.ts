6a717827c2dad9f7e3d0cf30c1f90fe8d0d1ded9
modestoartur <modestoartur@gmail.com>
2022-02-06 16:36:35 -0300

    Listagem de produtos no carrinho

--- src/app/pages/carrinho/carrinho.module.ts
+++ src/app/pages/carrinho/carrinho.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CarrinhoListagemComponent } from './carrinho-listagem/carrinho-listagem.component';
-
-
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   declarations: [CarrinhoListagemComponent],
-  imports: [
-    CommonModule
-  ]
+  imports: [BrowserModule, CommonModule],
 })
-export class CarrinhoModule { }
+export class CarrinhoModule {}
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
+import { CarrinhoModule } from './carrinho/carrinho.module';
 import { ProdutosModule } from './produtos/produtos.module';
 
 @NgModule({
-  imports: [
-    ProdutosModule
-  ],
-  exports: [
-    ProdutosModule
-  ],
+  imports: [ProdutosModule, CarrinhoModule],
+  exports: [ProdutosModule, CarrinhoModule],
 })
 export class PagesModule {}
