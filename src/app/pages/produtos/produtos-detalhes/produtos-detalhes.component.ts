5128c992026950a29e7b194bfbb9ec1d8675be38
modestoartur <modestoartur@gmail.com>
2022-02-06 03:25:27 -0300

    Inicio do componente

---
+++ src/app/pages/produtos/produtos-detalhes/produtos-detalhes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-produtos-detalhes',
+  templateUrl: './produtos-detalhes.component.html',
+  styleUrls: ['./produtos-detalhes.component.scss']
+})
+export class ProdutosDetalhesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
