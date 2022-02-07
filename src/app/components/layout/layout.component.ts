4927d46ee7ebaf132e19e1bffca8b25286fca2ae
modestoartur <modestoartur@gmail.com>
2022-02-07 00:01:54 -0300

    Upgrade para 13

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,19 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { AutenticacaoService } from '@app/core/services/autenticacao.service';
+import { Component } from '@angular/core';
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss'],
 })
-export class LayoutComponent implements OnInit {
-  constructor(
-    private router: Router,
-    public autenticacaoService: AutenticacaoService
-  ) {}
-  ngOnInit(): void {}
-  verificarLinkAtivo(link) {
-    link = link.slice(0, -1);
-    return this.router.url.startsWith(link);
-  }
+export class LayoutComponent {
+  constructor() {}
 }
--- src/app/components/layout/rodape/rodape.component.ts
+++ src/app/components/layout/rodape/rodape.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { version } from '../../../../../package.json';
 @Component({
   selector: 'app-rodape',
   templateUrl: './rodape.component.html',
   styleUrls: ['./rodape.component.scss'],
 })
 export class RodapeComponent {
-  public version: string = version;
   rotaPermitida: boolean;
   constructor(private router: Router) {}
   naoAtivo(link) {
